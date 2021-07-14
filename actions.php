97f73bebed9f670d8a2668350e4ab6aa17bc085a
Sudaox-realAlt <86860681+Sudaox-realAlt@users.noreply.github.com>
2021-07-14 22:13:32 +0100

    Update actions.php

--- actions.php
+++ actions.php
@@ -1,2 +1,21 @@
 <?php
 // For actions like: Like, Disklike, Comment
+// Like action
+if(isset($_GET['action'])) {
+if(isset($_GET["id"])){
+  if($_GET['id'] === "like") {
+if(file_exists("ids/" . $_GET["id"] . ".json")){
+	// File exists
+	$json = file_get_contents("ids/" . $_GET["id"] . ".json");
+	$jsonD = json_decode($json, true);
+	$jsonD["likes"] = $jsonD["likes"] + 1;
+	$ldFix = json_encode($jsonD, true);
+    file_put_contents("ids/" . $_GET["id"]. ".json", $ldFix);
+} else {
+	echo "[FAIL]: Video doesn't exist.";
+}
+} else {
+	echo "Why are you here.";
+}
+}
+}
