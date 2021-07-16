63ef1a61f83ce8ade9665102a2d910209941828e
HotPizzaYT <57576298+HotPizzaYT@users.noreply.github.com>
2021-07-16 15:59:33 -0500

    Import FFProbe

--- thb/generateTN.php
+++ thb/generateTN.php
@@ -7,7 +7,16 @@ require 'vendor/autoload.php';
 $host = "unknown";
 if(file_exists("bin/ffmpeg.exe") && file_exists("bin/ffprobe.exe"))
 {
+// Import FFMpeg
 $ffmpeg = FFMpeg\FFMpeg::create(array(
+    'ffmpeg.binaries'  => 'bin/ffmpeg.exe',
+    'ffprobe.binaries' => 'bin/ffprobe.exe',
+    'timeout'          => 3600, // The timeout for the underlying process
+    'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
+    
+));
+// Import FFProbe
+$ffprobe = FFMpeg\FFProbe::create(array(
     'ffmpeg.binaries'  => 'bin/ffmpeg.exe',
     'ffprobe.binaries' => 'bin/ffprobe.exe',
     'timeout'          => 3600, // The timeout for the underlying process
@@ -18,7 +27,18 @@ $host = "windows";
 } else {
 if(file_exists("/opt/local/ffmpeg/bin/ffmpeg") && file_exists("/opt/local/ffmpeg/bin/ffprobe")){
     // Linux host detected.
+
+    // Import FFMpeg
     $ffmpeg = FFMpeg\FFMpeg::create(array(
+    'ffmpeg.binaries'  => '/opt/local/ffmpeg/bin/ffmpeg',
+    'ffprobe.binaries' => '/opt/local/ffmpeg/bin/ffprobe',
+    'timeout'          => 3600, // The timeout for the underlying process
+    'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
+    
+));
+    
+    // Import FFProbe
+    $ffprobe = FFMpeg\FFProbe::create(array(
     'ffmpeg.binaries'  => '/opt/local/ffmpeg/bin/ffmpeg',
     'ffprobe.binaries' => '/opt/local/ffmpeg/bin/ffprobe',
     'timeout'          => 3600, // The timeout for the underlying process
@@ -44,9 +64,25 @@ if(isset($_GET["id"]) && file_exists("../ids/" . $_GET["id"] . ".json") && $host
     ->filters()
     ->resize(new FFMpeg\Coordinate\Dimension(320, 240))
     ->synchronize();
-    $video
-    ->frame(FFMpeg\Coordinate\TimeCode::fromSeconds(0.5))
-    ->save($_GET["id"] . '.jpg');
+
+    // Does request have custom timecode?
+    if(isset($_GET["tc"])){
+
+    // Custom timecode has been set.
+    
+        $video
+        ->frame(FFMpeg\Coordinate\TimeCode::fromSeconds($_GET["tc"]))
+        ->save($_GET["id"] . '.jpg');
+    } else {  
+        // $ffprobe = FFMpeg\FFProbe::create();
+        $duration = $ffprobe->format($src)->get('duration');
+        // Try to get frame that's in the middle of the video
+        $middle = $duration / 2;
+        echo $middle;
+        $video
+        ->frame(FFMpeg\Coordinate\TimeCode::fromSeconds($middle))
+        ->save($_GET["id"] . '.jpg');
+    }
     echo "<br />Saved the thumbnail. Here it is:<br /> <img src='" . $_GET["id"] . ".jpg' />";
 } else {
     if(isset($_GET["id"]) && $host === "error"){
@@ -66,59 +102,3 @@ if(isset($_GET["id"]) && file_exists("../ids/" . $_GET["id"] . ".json") && $host
 
 
 // API
-function thumbRequest($id){
-    if(file_exists("bin/ffmpeg.exe") && file_exists("bin/ffprobe.exe"))
-{
-$ffmpeg = FFMpeg\FFMpeg::create(array(
-    'ffmpeg.binaries'  => 'bin/ffmpeg.exe',
-    'ffprobe.binaries' => 'bin/ffprobe.exe',
-    'timeout'          => 3600, // The timeout for the underlying process
-    'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
-    
-));
-$host = "windows";
-} else {
-if(file_exists("/opt/local/ffmpeg/bin/ffmpeg") && file_exists("/opt/local/ffmpeg/bin/ffprobe")){
-    // Linux host detected.
-    $ffmpeg = FFMpeg\FFMpeg::create(array(
-    'ffmpeg.binaries'  => '/opt/local/ffmpeg/bin/ffmpeg',
-    'ffprobe.binaries' => '/opt/local/ffmpeg/bin/ffprobe',
-    'timeout'          => 3600, // The timeout for the underlying process
-    'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
-    
-));
-$host = "linux";
-} else {
-
-    $host = "error";
-}
-}
-
-if(isset($id) && file_exists("ids/" . $id . ".json") && $host !== "error"){
-	$jsonPath = "ids/" . $id . ".json";
-    $json = file_get_contents($jsonPath);
-	$jsonD = json_decode($json, true);
-    $src = $jsonD["src"];
-    // echo "Opening " . $src;
-	// $ffmpeg = FFMpeg\FFMpeg::create();
-    $video = $ffmpeg->open($src);
-    $video
-    ->filters()
-    ->resize(new FFMpeg\Coordinate\Dimension(320, 240))
-    ->synchronize();
-    $video
-    ->frame(FFMpeg\Coordinate\TimeCode::fromSeconds(0.5))
-    ->save("/thb" . $id . '.jpg');
-    // echo "<br />Saved the thumbnail. Here it is:<br /> <img src='" . $id . ".jpg' />";
-} else {
-    if(isset($_GET["id"]) && $host === "error"){
-    echo "<font color='red'>[ERROR]:</font> FFMpeg and FFProbe libraries not found! Please install them. If you are using Windows, please put these libraries in bin/. These libraries on Windows are around 110MB each.";
-    } else {
-        if(isset($_GET["id"]) && $host !== "error"){
-            echo "<font color='red'>[ERROR]:</font> That video ID has not been found!";
-        } else {
-        // echo "What are you doing here?";
-        }
-    }
-}
-}
