12b48674d979b11e83cf75896a8f1712171d1e25
HotPizzaYT <57576298+HotPizzaYT@users.noreply.github.com>
2021-07-17 20:43:28 -0500

    Properly credit myself

--- credits.php
+++ credits.php
@@ -2,7 +2,7 @@
 <h1>CREDITS</h1>
 <?php
 $people = array();
-$people['0'] = "HxOr1337 - Making most things";
+$people['0'] = "HxOr1337./HotPizzaYT/(*DripDog*) - Making most things";
 $people['1'] = "Sudaox - Helping out";
 $pcount = count($people);
 for($x = 0; $x < $pcount; $x++){
