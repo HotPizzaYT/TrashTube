e3b08280074f8ec05f79f0090b0b9a9235aedbe9
HotPizzaYT <57576298+HotPizzaYT@users.noreply.github.com>
2021-07-17 20:40:45 -0500

    Update credits.php

--- credits.php
+++ credits.php
@@ -1,6 +1,11 @@
 <!-- Special thanks to Sudaox uwu -->
+<h1>CREDITS</h1>
 <?php
 $people = array();
-$people['1'] = "HxOr1337 - Making most things";
-$people['2'] = "Sudaox - Helping out";
+$people['0'] = "HxOr1337 - Making most things";
+$people['1'] = "Sudaox - Helping out";
+$pcount = count($people);
+for($x = 0; $x < $pcount; $x++){
+  echo $people[$x] . "<br />";
+}
 ?>
