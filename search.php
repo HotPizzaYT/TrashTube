87d8659fa61e5de1e7aef4078d80bb7b3f58a6a0
Sudaox <75401250+sudaox-real@users.noreply.github.com>
2021-07-27 03:13:35 +0100

    Overhaul of search. Added special searches (loc:, channel:all) and made no ?q= show searchbox

--- search.php
+++ search.php
@@ -1,4 +1,80 @@
 <?php
+if(!isset($_GET['q'])) {
+    echo('<html>
+   <head>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Search</title><script>
+   function checkEnter(event, fun) {
+       if (event.keyCode == 13) {
+           fun();
+       }
+   }
+   function search() {
+try {
+   x = document.getElementById("sch").value;
+
+// Do something with X.
+console.log(x);
+window.location = `search.php?q=${x}`
+}
+catch {
+   window.location = "error.php?c=3";
+}
+   }
+</script>
+</head>
+<body>
+<center class="block">
+Search : <input type="text" id="sch" class="search" style="width: 500px" onkeydown="checkEnter(event, search)" placeholder="Search for videos..." />
+</center>
+</body>
+</html>');
+die();
+}
+$s = substr($_GET['q'], 0, 4);
+$chn = substr($_GET['q'], 0, 9);
+$chn2 = substr($_GET['q'], 0, 8);
+if($s === "loc:") {
+    $newquery = $_GET['q'];
+    $newquery = str_ireplace("loc:", "?q=", $newquery);
+  header("Location: location.php$newquery");
+ //  $cn = file_get_contents("location.php$newquery");
+    die();
+}
+if($chn === "channels:") {
+    $newquery = $_GET['q'];
+    $newquery = str_ireplace("channels:", "", $newquery);
+    if($newquery === "all") {
+        header("Location: channels.php");
+    }
+    else {
+        die("No results.");
+    }
+}
+if($chn === "channel:") {
+    $newquery = $_GET['q'];
+    $newquery = str_ireplace("channel:", "", $newquery);
+       if(file_exists("accounts/data/$newquery.json")) {
+           header("Location: channel.php?id=$newquery");
+       }
+       else {
+           die("No results.");
+       }
+}
+//die($s);
+if(!file_exists("config.json")) {
+    die("Error loading configuration. Please reinstall.");
+}
+else {
+    $config = json_decode(file_get_contents("config.json"), true);
+    if($config['installed'] === "no") {
+        header("Location: install.php?page=welcome");
+    }
+    $name = $config['name'];
+    $path = $config['path'];
+}
+    ?>
+<?php
        $dir = "ids/";
        $videos = scandir($dir);
        // var_dump($videos);
@@ -14,19 +90,22 @@
           // echo $num;
           // $x2 = $num;
           // echo($x2);
-          $data['title'] = str_ireplace("'", "", $data['title']);
-          $data['title'] = str_ireplace(" ", "", $data['title']);
+     //     $data['title'] = str_ireplace("'", "", $data['title']);
+        //  $data['title'] = str_ireplace(" ", "", $data['title']);
+        $data['title'] = str_replace(array("\r", "\n", "\r\n", "\v", "\t", "\0","\x"), "", $data['title']);
+        $q = $data['title'];
 if (strpos($data['title'], $_GET['q']) !== false) {
    // echo $id . ": " . $data['title'] . "<br>";
    $GLOBALS['result'] = "yes";
    if(file_exists("thb/" . $id . ".jpg")){
 
     // Changed where link goes!
-    echo "<div class='video-tile'><a href='view.php?id=" . $id . "'><img src='thb/" . $id . ".jpg' width='320' height='240' /><br />" . htmlspecialchars($data["title"]) . " (" . $data["views"] . " views)</a></div>";
+    echo "<div class='video-tile'><a href='view.php?id=" . $id . "'><img src='thb/" . $id . ".jpg' width='320' height='240' /><br />" . $data["title"] . " (" . $data["views"] . " views)</a></div>";
     } else {
-        echo "<div class='video-tile'><a title='This video has no thumbnail!' href='view.php?id=" . $id . "'>" . htmlspecialchars($data["title"]) . " (" . $data["views"] . " views)</a></div>";
+        echo "<div class='video-tile'><a title='This video has no thumbnail!' href='view.php?id=" . $id . "'>" . $data["title"] . " (" . $data["views"] . " views)</a></div>";
     }
 }
+
 else {
     if(@$GLOBALS['result'] === "yes") {
 
